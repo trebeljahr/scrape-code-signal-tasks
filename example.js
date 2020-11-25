552c38215c6e4fc710410f1a10525e9e4bcd8051
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-11-25 12:13:45 +0100

    Add login functionality

--- example.js
+++ example.js
@@ -1,14 +1,5 @@
 const puppeteer = require("puppeteer");
 
-const clickButtonWithText = async (page, text) => {
-  const [button] = await page.$x(`//button[contains(., ${text})]`);
-  if (button) {
-    await button.click();
-  } else {
-    throw new Error(`Button with "${text}" as text not found!`);
-  }
-};
-
 const url = "https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ";
 
 (async () => {
@@ -16,26 +7,30 @@ const url = "https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ";
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: "networkidle0" });
+  await page.screenshot({ path: "loaded-page-screenshot.png" });
 
-  await page.screenshot({ path: "login-debug-screenshot.png" });
-  await clickButtonWithText(page, "Log In");
-  await trySigningIn(page);
+  const logIN =
+    "body > div:nth-child(11) > div > header > nav > div > div.-layout-h.-center.-space-h-8.-padding-h-16 > div.button.-type-primary.-size-28";
+  await page.click(logIN);
+  await page.screenshot({ path: "clicked-login-button-screenshot.png" });
+
+  await enterCredentials(page);
+  await page.screenshot({ path: "entered-credentials-screenshot.png" });
+
+  const signIN =
+    "body > div:nth-child(11) > div > main > div > div > div > div > form > div.-layout-v.-space-v-16 > div.-layout-v.-space-v-8 > div > div";
+  await page.click(signIN);
+  await page.screenshot({ path: "signed-in-screenshot.png" });
+
   await browser.close();
 })();
 
-async function trySigningIn(page) {
+async function enterCredentials(page) {
   console.log("Trying to log in!");
-  await page.type("input[name=password]", "test comment", {
+  await page.type("input[name=password]", process.env.PASSWORD, {
     delay: 20,
   });
-  await page.type("input[name=username]", "test comment", {
+  await page.type("input[name=username]", process.env.USER_NAME, {
     delay: 20,
   });
-
-  const signInButton = await page.$(
-    ".button--content .-layout-h .-center-center .-space-h-8"
-  );
-  if (signInButton) {
-    await signInButton.click();
-  }
 }
